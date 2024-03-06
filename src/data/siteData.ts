9d304da43b25da5d4416c2b7547cfa5e68465919
hamm44 <mrbighit@gmail.com>
2024-03-06 11:49:03 +1100

    lesson44

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -2,3 +2,31 @@ export const baseData = {
   title: "Mike Portfolio Site",
   description: "Welcome to my portfolio site! I'm a self-taught developer obssessed with quality.",
 };
+
+export const socialData = [
+  {
+    title: "dribbble",
+    href: "https://dribbble.com/",
+    icon: "icon--dribbble",
+  },
+  {
+    title: "instagram",
+    href: "https://instagram.com/",
+    icon: "icon--instagram",
+  },
+  {
+    title: "twitter",
+    href: "https://twitter.com/",
+    icon: "icon--twitter",
+  },
+  {
+    title: "youtube",
+    href: "https://youtube.com/",
+    icon: "icon--youtube",
+  },
+  {
+    title: "github",
+    href: "https://github.com/",
+    icon: "icon--github",
+  },
+];
